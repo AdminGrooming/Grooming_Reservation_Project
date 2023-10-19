0c563febaebf8f02d71d287389fbc6f1e482517b
padmapriya <padmapriyarajabe@gmail.com>
2023-10-19 19:15:12 +0530

    merge

--- src/main/java/com/edu/grooming/GroomingReservationSystemApplication.java
+++ src/main/java/com/edu/grooming/GroomingReservationSystemApplication.java
@@ -8,8 +8,12 @@ public class GroomingReservationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroomingReservationSystemApplication.class, args);
+
 		//new changes to the github
 		//grooming reservation system
+
+		
+
 	}
 
 }
