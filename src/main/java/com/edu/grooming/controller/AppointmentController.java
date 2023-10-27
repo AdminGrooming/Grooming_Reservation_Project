28f3a595ec64e2b7157c9ff25d7506597e88d41f
Surendharan Baskaran <surendharanbe@gmail.com>
2023-10-27 23:15:21 +0530

    Update AppointmentController.java

--- src/main/java/com/edu/grooming/controller/AppointmentController.java
+++ src/main/java/com/edu/grooming/controller/AppointmentController.java
@@ -28,28 +28,24 @@ public class AppointmentController {
 	public List<Appointment> getAllAppointments() {
 		return appointmentService.getAllAppointments();
 	}
-	
+
 	@PutMapping("/updateAppointmentUser/{userid}/{appointmentid}")
 	public Appointment updateAppointmentUser(@PathVariable("userid") Integer userid,@PathVariable("appointmentid") Integer appointmentId) {
 		return appointmentService.updateAppointmentUser(userid,appointmentId);
-		
 	}
-	
+
 	@PutMapping("/updateAppointmentSalon/{salonid}/{appointmentid}")
 	public Appointment updateAppointmentSalon(@PathVariable("salonid") Integer salonid,@PathVariable("appointmentid") Integer appointmentId) {
 		return appointmentService.updateAppointmentSalon(salonid,appointmentId);
-		
 	}
-	
+
 	@PutMapping("/updateAppointmentStylist/{stylistid}/{appointmentid}")
 	public Appointment updateAppointmentStylist(@PathVariable("stylistid") Integer stylistid,@PathVariable("appointmentid") Integer appointmentId) {
 		return appointmentService.updateAppointmentStylist(stylistid,appointmentId);
-		
 	}
-	
+
 	@PutMapping("/updateAppointmentService/{serviceid}/{appointmentid}")
 	public Appointment updateAppointmentService(@PathVariable("serviceid") Integer serviceid,@PathVariable("appointmentid") Integer appointmentId) {
 		return appointmentService.updateAppointmentService(serviceid,appointmentId);
 	}
-	
 }
